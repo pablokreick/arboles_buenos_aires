ccc31d8c8d17d6340685b1024d482ce7306d117b
pablokreick <pablokreick@gmail.com>
2025-12-02 10:31:20 -0300

    fix: update GitHub anchor

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -2,7 +2,13 @@ const Footer = () => {
 	return (
 		<footer className="border-t border-gray-200 bg-white pt-16 pb-4 text-gray-400">
 			<div className="flex justify-center items-center flex-col gap-4 text-center">
-				<a href="#">GitHub</a>
+				<p>
+					Código fuente del proyecto en{" "}
+					<a href="https://github.com/pablokreick/arboles_buenos_aires.git" target="_blank" rel="noopener noreferrer">
+						GitHub
+					</a>
+					.
+				</p>
 				<p className="text-base">&copy; 2025 Arbolado Buenos Aires API. Proyecto Open Source.</p>
 			</div>
 		</footer>
